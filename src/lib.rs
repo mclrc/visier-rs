b7ae6533b0f4895ea20a4a6cefb9c61057a7b8dc
mclrc <moritzamando@proton.me>
2024-04-23 18:01:06 +0200

    Use client struct, no builder

--- src/lib.rs
+++ src/lib.rs
@@ -1,197 +1,135 @@
+use serde::de::DeserializeOwned;
 use serde_json::{json, Map, Value};
 use std::error::Error;
 
-const VIZIER_TAP_URL: &str = "http://tapvizier.u-strasbg.fr/TAPVizieR/tap/sync";
+const DEFAULT_VIZIER_TAP_URL: &str = "http://tapvizier.u-strasbg.fr/TAPVizieR/tap/sync";
 
-type QueryResult = Vec<Value>;
-
-fn parse_query_result(data: Value) -> Result<QueryResult, Box<dyn Error>> {
-    let columns = data
-        .get("metadata")
-        .ok_or("Metadata not found")?
-        .as_array()
-        .ok_or("Metadata is not an array")?;
-    let data = data.get("data").ok_or("Data not found")?;
-
-    let mut column_names = Vec::new();
-    for column in columns {
-        let name = column.get("name").ok_or("Column name not found")?;
-        column_names.push(name.as_str().ok_or("Column name is not a string")?);
-    }
-
-    let mut result = Vec::new();
-    for row in data.as_array().ok_or("Data is not an array")? {
-        let mut row_data = Map::new();
-
-        for (i, value) in row
-            .as_array()
-            .ok_or("Row is not an array")?
-            .iter()
-            .enumerate()
-        {
-            row_data.insert(column_names[i].to_string(), value.clone());
-        }
-        result.push(Value::Object(row_data));
-    }
-
-    Ok(result)
+pub struct Client {
+    tap_url: String,
+    http_client: reqwest::Client,
 }
 
-pub async fn query_vizier(adql_query: &str) -> Result<QueryResult, Box<dyn Error>> {
-    let request_body = json!({
-        "request": "doQuery",
-        "lang": "ADQL",
-        "format": "json",
-        "query": adql_query
-    });
+impl Client {
+    pub fn new(tap_url: &str) -> Self {
+        Self {
+            tap_url: tap_url.to_string(),
+            http_client: reqwest::Client::new(),
+        }
+    }
 
-    let response = {
-        let client = reqwest::Client::new();
+    pub async fn query<T: DeserializeOwned>(
+        &self,
+        adql_query: &str,
+    ) -> Result<Vec<T>, Box<dyn Error>> {
+        let request_query = json!({
+            "request": "doQuery",
+            "lang": "ADQL",
+            "format": "json",
+            "query": adql_query
+        });
 
-        client
-            .get(VIZIER_TAP_URL)
-            .query(&request_body)
+        let response = self
+            .http_client
+            .get(&self.tap_url)
+            .query(&request_query)
             .send()
-            .await?
-    };
+            .await?;
 
-    if response.status().is_success() {
-        let data = response.json::<Value>().await?;
-        let parsed_data = parse_query_result(data)?;
-        Ok(parsed_data)
-    } else {
-        Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Failed to fetch data",
-        )))
+        if response.status().is_success() {
+            let data = response.json::<Value>().await?;
+            let parsed_data = Client::parse_query_result::<T>(data)?;
+            Ok(parsed_data)
+        } else {
+            Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Failed to fetch data",
+            )))
+        }
+    }
+
+    fn parse_query_result<T: DeserializeOwned>(data: Value) -> Result<Vec<T>, Box<dyn Error>> {
+        let columns = data
+            .get("metadata")
+            .ok_or("Metadata not found")?
+            .as_array()
+            .ok_or("Metadata is not an array")?;
+        let data = data.get("data").ok_or("Data not found")?;
+
+        let mut column_names = Vec::new();
+        for column in columns {
+            let name = column.get("name").ok_or("Column name not found")?;
+            column_names.push(name.as_str().ok_or("Column name is not a string")?);
+        }
+
+        let mut result = Vec::new();
+        for row in data.as_array().ok_or("Data is not an array")? {
+            let mut row_data = Map::new();
+
+            for (i, value) in row
+                .as_array()
+                .ok_or("Row is not an array")?
+                .iter()
+                .enumerate()
+            {
+                row_data.insert(column_names[i].to_string(), value.clone());
+            }
+            result.push(serde_json::from_value(Value::Object(row_data))?);
+        }
+
+        Ok(result)
     }
 }
 
-#[cfg(test)]
-#[tokio::test]
-async fn query_vizier_test() {
-    let adql_query = "SELECT TOP 100 * FROM \"I/261/fonac\"";
-    let result = query_vizier(adql_query).await.unwrap();
-    assert!(result.len() == 100);
-}
-
-pub trait SelectClause {
-    fn select(&self) -> &str;
-}
-
-impl SelectClause for &str {
-    fn select(&self) -> &str {
-        self
-    }
-}
-
-pub trait FromClause {
-    fn from(&self) -> &str;
-}
-
-impl FromClause for &str {
-    fn from(&self) -> &str {
-        self
-    }
-}
-
-pub trait WhereClause {
-    fn where_(&self) -> &str;
-}
-
-impl WhereClause for &str {
-    fn where_(&self) -> &str {
-        self
-    }
-}
-
-impl WhereClause for () {
-    fn where_(&self) -> &str {
-        ""
-    }
-}
-
-pub struct Query<S, F, W> {
-    select: S,
-    from: F,
-    where_: W,
-}
-
-impl Default for Query<(), (), ()> {
+impl Default for Client {
     fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl Query<(), (), ()> {
-    pub fn new() -> Self {
-        Query {
-            select: (),
-            from: (),
-            where_: (),
-        }
-    }
-}
-
-impl<S, F, W> Query<S, F, W> {
-    pub fn select<S2: SelectClause>(self, select: S2) -> Query<S2, F, W> {
-        Query {
-            select,
-            from: self.from,
-            where_: self.where_,
-        }
-    }
-}
-
-impl<S, F, W> Query<S, F, W> {
-    pub fn from<F2: FromClause>(self, from: F2) -> Query<S, F2, W> {
-        Query {
-            select: self.select,
-            from,
-            where_: self.where_,
-        }
-    }
-}
-
-impl<S, F, W> Query<S, F, W> {
-    pub fn filter<W2: WhereClause>(self, filter: W2) -> Query<S, F, W2> {
-        Query {
-            select: self.select,
-            from: self.from,
-            where_: filter,
-        }
-    }
-}
-
-impl<S, F, W> Query<S, F, W>
-where
-    S: SelectClause,
-    F: FromClause,
-    W: WhereClause,
-{
-    pub async fn send(self) -> Result<QueryResult, Box<dyn Error>> {
-        let adql_query = format!(
-            "{} {} {}",
-            self.select.select(),
-            self.from.from(),
-            self.where_.where_()
-        );
-
-        query_vizier(&adql_query).await
+        Self::new(DEFAULT_VIZIER_TAP_URL)
     }
 }
 
 #[cfg(test)]
-#[tokio::test]
-async fn query_builder_test() {
-    let response = Query::new()
-        .select("SELECT TOP 100 *")
-        .from("FROM \"I/261/fonac\"")
-        .send()
-        .await
-        .unwrap();
+mod tests {
+    use serde::Deserialize;
 
-    println!("{:?}", response);
+    use super::*;
 
-    assert!(response.len() == 100)
+    #[tokio::test]
+    async fn query_test() {
+        let adql_query = "SELECT TOP 100 * FROM \"I/261/fonac\"";
+
+        let client = Client::default();
+
+        let result = client.query::<Value>(adql_query).await.unwrap();
+
+        assert!(result.len() == 100);
+    }
+
+    #[derive(Deserialize, Debug)]
+    #[allow(non_snake_case, dead_code)]
+    struct QueryResult {
+        AC2000: i32,
+        ACT: Option<i32>,
+        #[serde(rename = "B-R")]
+        BR: Option<f64>,
+        #[serde(rename = "B-V")]
+        BV: Option<f64>,
+        Bmag: f64,
+        DEJ2000: f64,
+        #[serde(rename = "Ep-1900")]
+        Ep1900: f64,
+        Qflag: Option<i32>,
+        RAJ2000: f64,
+        pmDE: f64,
+        pmRA: f64,
+        q_Bmag: Option<i32>,
+        recno: i32,
+    }
+
+    #[tokio::test]
+    async fn query_test_typed() {
+        let client = Client::default();
+
+        let adql_query = "SELECT TOP 100 * FROM \"I/261/fonac\"";
+
+        client.query::<QueryResult>(adql_query).await.unwrap();
+    }
 }
